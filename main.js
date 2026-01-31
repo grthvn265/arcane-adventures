9af73a25b84778d609457deda7bb8fb3782f737c
Aaron Arevalo <arevalo.aaroncarl26@gmail.com>
2026-02-01 00:07:00 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three';
-import { Game } from 'game';
-
-// Get the render target
-const renderDiv = document.getElementById('renderDiv');
-
-// Initialize and start the game
-const game = new Game(renderDiv);
-game.start();
